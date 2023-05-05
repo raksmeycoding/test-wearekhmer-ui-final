31860c250230edd025176ea8bb423dbca0eeb0ca
raksmey koung <raksmeykoung@gmail.com>
2023-05-06 03:31:07 +0700

    strcuture: organize react porject structure

---
+++ src/redux/selectors/homeSelector.js
@@ -0,0 +1,7 @@
+import { createSelector } from 'reselect'
+// selector
+const getHomeState = (state) => state.homeState.home
+// reselect function
+export const getHomeSelector = createSelector([getHomeState], (home) => home)
+
+export default getHomeSelector
